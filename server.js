1b8c1279c3fb868c484c99a3dbf233b82072172b
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-14 15:51:09 +0000

    Added post functionality with multer and HTML form

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var multer = require("multer");
+var path = require('path');
+
+var upload = multer({dest:'uploads'});
+
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 })
 
+app.post('/uploads', upload.single('file-input'), function(req, res) {
+    if (req) {
+        res.send('Hi!');
+    } else {
+        res.send('No');
+    }
+});
+
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port!')
+  console.log('Example app listening on port!');
 })
